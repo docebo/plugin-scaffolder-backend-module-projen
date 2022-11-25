27fa7d41057429bad88a00db806b8ee8bb9bc571
Fabio Vitali <fabio.vitali@docebo.com>
2022-11-25 12:09:53 +0100

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,8 @@
-const { typescript } = require('projen');
-const project = new typescript.TypeScriptProject({
+const { javascript, typescript } = require('projen');
+const project = new typescript.TypeScriptAppProject({
   defaultReleaseBranch: 'main',
   name: 'plugin-scaffolder-backend-module-projen',
+  packageManager: javascript.NodePackageManager.NPM,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
