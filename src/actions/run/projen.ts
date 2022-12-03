d8f38b76cf152637a934550c32bebf8e13e62acc
Fabio Vitali <fabio.vitali@docebo.com>
2022-12-03 12:34:36 +0100

    fix: add no-synth arg

--- src/actions/run/projen.ts
+++ src/actions/run/projen.ts
@@ -8,7 +8,7 @@ export function createRunProjenAction() {
     async handler(ctx) {
       try {
         ctx.input.projectType
-          ? await runShellCommand(`npx projen@latest new ${ctx.input.projectType} --package-manager NPM --outdir "${ctx.workspacePath}"`)
+          ? await runShellCommand(`npx projen@latest new ${ctx.input.projectType} --package-manager NPM --no-synth --outdir "${ctx.workspacePath}"`)
           : await runShellCommand(`cd "${ctx.workspacePath}" && npm i ts-node@10 && npx projen@latest`);
       } catch (e) {
         console.error(e);
