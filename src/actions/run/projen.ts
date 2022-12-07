7b70d0926d429c7f0beb83805305dc6e1254a997
Fabio Vitali <fabio.vitali@docebo.com>
2022-12-07 17:39:14 +0100

    fix: useless logs removed

--- src/actions/run/projen.ts
+++ src/actions/run/projen.ts
@@ -6,15 +6,10 @@ export function createRunProjenAction() {
     id: 'run:projen',
     description: 'Runs Projen',
     async handler(ctx) {
-      ctx.logger.info(ctx.input.projectType);
-      ctx.logger.info(JSON.stringify(ctx.input));
-      ctx.logger.info(JSON.stringify(ctx.workspacePath));
       try {
-        ctx.logger.info('----start');
         ctx.input.projectType
           ? await runShellCommand(`npx projen@latest new ${ctx.input.projectType} --package-manager NPM --outdir "${ctx.workspacePath}"`)
           : await runShellCommand(`cd "${ctx.workspacePath}" && npm i ts-node@latest && npx projen@latest`);
-        ctx.logger.info('----end');
       } catch (e) {
         ctx.logger.error(e);
       }
