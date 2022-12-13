3e203d38f8c769917255aae27e48334da3393157
Fabio Vitali <fabio.vitali@docebo.com>
2022-12-13 12:03:12 +0100

    fix: run commands sequentially

--- src/actions/run/projen.ts
+++ src/actions/run/projen.ts
@@ -6,20 +6,26 @@ export function createRunProjenAction() {
     id: 'run:projen',
     description: 'Runs Projen',
     async handler(ctx) {
+      const commands = ctx.input.projectType
+        ? ['npm i -g projen@latest', `npx projen new ${ctx.input.projectType} --package-manager NPM --no-synth --outdir "${ctx.workspacePath}"`]
+        : [`cd "${ctx.workspacePath}" && npm install ts-node@^10`, `cd "${ctx.workspacePath}" && npx projen`];
+
       try {
-        ctx.input.projectType
-          ? await runShellCommand(`npx projen@latest new ${ctx.input.projectType} --package-manager NPM --outdir "${ctx.workspacePath}"`)
-          : await runShellCommand(`cd "${ctx.workspacePath}" && npm i ts-node@latest && npx projen@latest`);
+        for (const command of commands) {
+          ctx.logger.info(`Command: ${command}`);
+          const result = await runShellCommand(command);
+          ctx.logger.info(`Result: ${result}`);
+        }
       } catch (e) {
-        ctx.logger.error(e);
+        ctx.logger.error(`Error: ${e}`);
       }
     }
   });
 }
 
-const runShellCommand = async (cmd: string) =>
+const runShellCommand = async (command: string) =>
   new Promise((resolve, reject) => {
-    exec(cmd, { maxBuffer: 1024 * 1000 }, (error, stdout, stderr) => {
-      error ? reject(error) : resolve(stdout? stdout : stderr);
+    exec(command, { maxBuffer: 1024 * 1000 }, (error, stdout, stderr) => {
+      error ? reject(error) : resolve(stdout ? stdout : stderr);
     });
   });
